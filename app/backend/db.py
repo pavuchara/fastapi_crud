4c6be592783566991d0025892d2754a8b771d0c2
Marsel <lllmarselll@gmail.com>
2024-10-01 16:17:41 +0300

    Prepare for postgres

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,25 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+load_dotenv()
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 
-engine = create_engine("sqlite:///ecommerce.db", echo=True)
-SessionLocal = sessionmaker(bind=engine)
+URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
+
+
+engine = create_async_engine(URL, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
